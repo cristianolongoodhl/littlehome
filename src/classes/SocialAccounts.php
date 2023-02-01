f01eea942208d6c1c623ea0a3cb453a996ac5761
Cristiano Longo <cristianolongo@opendatahacklab.org>
2023-02-01 09:37:43 +0100

    #1 telegram account

--- src/classes/SocialAccounts.php
+++ src/classes/SocialAccounts.php
@@ -7,12 +7,13 @@ class SocialAccounts{
 	public $presentations;
 
 	function __construct(){
-		$this->services=array('http://www.facebook.com','http://www.twitter.com');
+	    $this->services=array('http://www.facebook.com','http://www.twitter.com','https://github.com', 'http://telegram.org');
 		$this->presentations = array(
 			'http://www.facebook.com' => new AccountPresentation('icons/iconfinder_facebook_alt_115478.png','facebook','facebook_page_name','facebook_page_url'),
 			'http://www.twitter.com' => new AccountPresentation('icons/iconfinder_02_111031.png','twitter','twitter_name','twitter_feed_url'),
 			'https://github.com' => new AccountPresentation('icons/GitHub-Mark-120px-plus.png','Organizzazione su GitHub','github_name','github_url'),
-			'https://www.youtube.com' => new AccountPresentation('icons/youtube.png','Canale Youtube','youtube_name','youtube_url')
+		    'https://www.youtube.com' => new AccountPresentation('icons/youtube.png','Canale Youtube','youtube_name','youtube_url'),
+		    'http://telegram.org' => new AccountPresentation('icons/Font_Awesome_5_brands_telegram-plane.svg','Canale Telegram','telegram_name','telegram_url')
 		);
 	}
 
