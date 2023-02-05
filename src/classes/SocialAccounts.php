675a4cd4ef765ed0c6a1997d421ebcff9695badd
Cristiano Longo <cristianolongo@opendatahacklab.org>
2023-02-05 17:11:17 +0100

    #2 #4 add mailing list and log access to rss feed

--- src/classes/SocialAccounts.php
+++ src/classes/SocialAccounts.php
@@ -9,6 +9,7 @@ class SocialAccounts{
 	function __construct(){
 	    $this->services=array('http://www.facebook.com','http://www.twitter.com','https://github.com', 'http://telegram.org');
 		$this->presentations = array(
+				    'https://noise.autistici.org' => new AccountPresentation('https://upload.wikimedia.org/wikipedia/commons/8/8b/PICOL_icon_Mail.svg','News Letter su A/I','newsletter_name','newsletter_url'),
 			'http://www.facebook.com' => new AccountPresentation('icons/iconfinder_facebook_alt_115478.png','facebook','facebook_page_name','facebook_page_url'),
 			'http://www.twitter.com' => new AccountPresentation('icons/iconfinder_02_111031.png','twitter','twitter_name','twitter_feed_url'),
 			'https://github.com' => new AccountPresentation('icons/GitHub-Mark-120px-plus.png','Organizzazione su GitHub','github_name','github_url'),
