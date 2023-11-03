b3d3c5cd74d83b509839821dbab70c3dff1f2937
Cristiano Longo <cristianolongo@opendatahacklab.org>
2023-11-03 22:12:06 +0100

    #18 add from URL parameter to discern visits coming from the RSS feed

--- src/rss1feed.php
+++ src/rss1feed.php
@@ -38,9 +38,8 @@ $l->readFromFile('../'.ARTICLES_FILE) or die('unable to read ../'.ARTICLES_FILE)
 if (isset($l->json->{'rss:items'}->{'rdf:li'}))
 	foreach($l->json->{'rss:items'}->{'rdf:li'} as $a){
 		$url=$a->{'@id'};
-		$date=DateTime::createFromFormat(Articles::W3CDATE, $a->{'dc:date'})->format('d/m/Y');
 		if (!$c->utils->isAbsoluteURL($url))
-			$url=BASE_URL.'src/viewArticle.php?url='.urlencode('../'.$url);
+			$url=BASE_URL.'src/viewArticle.php?url='.urlencode('../'.$url).'&from=rss';
 		$f->add($url,$a->{'rss:title'},$a->{'dc:date'});
 	}
 
